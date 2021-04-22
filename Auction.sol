dc86f0d3315d6bf54a04db2bbfdc7e1326cc79ac
Trung Duyen <trungduyen0220@gmail.com>
2021-04-22 22:45:17 +0700

    fix

--- Auction.sol
+++ Auction.sol
@@ -29,21 +29,14 @@ contract Auction {
 
     function Auction() public payable{    //constructor
                 
-        //Part 1 Task 1. Initialize beneficiary with address of smart contract’s owner
-        //Hint. In the constructor,"msg.sender" is the address of the owner.
-
-        // ** Start code here. 1 line approximately. **/
+        //Initialize beneficiary with address of smart contract’s owner
         beneficiary = msg.sender;
-        //** End code here. **/
 
         uint[] memory emptyArray;
         items[0] = Item({itemId:0,itemTokens:emptyArray});
-        
-        //Part 1 Task 2. Initialize two items with at index 1 and 2. 
-        // ** Start code here. 2 lines approximately. **/
         items[1] = Item({itemId:1,itemTokens:emptyArray});
         items[2] = Item({itemId:2,itemTokens:emptyArray});
-        //** End code here**/
+
     }
     
 
@@ -52,12 +45,9 @@ contract Auction {
         
         bidders[bidderCount].personId = bidderCount;
         
-        //Part 1 Task 3. Initialize the address of the bidder 
-        /*Hint. Here the bidders[bidderCount].addr should be initialized with address of the registrant.*/
-
-        // ** Start code here. 1 line approximately. **/
+        //Initialize the address of the bidder
         bidders[bidderCount].addr = msg.sender;
-        //** End code here. **
+       
         
         bidders[bidderCount].remainingTokens = 5; // only 5 tokens
         tokenDetails[msg.sender]=bidders[bidderCount];
@@ -73,27 +63,18 @@ contract Auction {
         */
         
         /*
-        Part 1 Task 4. Implement the three conditions below.
-            4.1 If the number of tokens remaining with the bidder is < count of tokens bidded, revert.
-            4.2 If there are no tokens remaining with the bidder, revert.
-            4.3 If the id of the item for which bid is placed, is greater than 2, revert.
-
-        Hint: "tokenDetails[msg.sender].remainingTokens" gives the details of the number of tokens remaining with the bidder.
+			If the number of tokens remaining with the bidder is < count of tokens bidded, revert.
+			If there are no tokens remaining with the bidder, revert.
+			If the id of the item for which bid is placed, is greater than 2, revert.
         */
         
-        // ** Start code here. 2 lines approximately. **/
         if (tokenDetails[msg.sender].remainingTokens < _count) revert();
         if (tokenDetails[msg.sender].remainingTokens == 0) revert();
         if (_itemId > 2) revert();
-        //** End code here. **
         
-        /*Part 1 Task 5. Decrement the remainingTokens by the number of tokens bid and store the value in balance variable.
-        Hint. "tokenDetails[msg.sender].remainingTokens" should be decremented by "_count". */
- 
-        // ** Start code here. 1 line approximately. **
+        /*Decrement the remainingTokens by the number of tokens bid and store the value in balance variable.*/
         uint balance = tokenDetails[msg.sender].remainingTokens - _count;
-        //** End code here. **
-        
+
         tokenDetails[msg.sender].remainingTokens=balance;
         bidders[tokenDetails[msg.sender].personId].remainingTokens=balance;//updating the same balance in bidders map.
         
@@ -103,13 +84,10 @@ contract Auction {
         }
     }
     
-    // Part 2 Task 1. Create a modifier named "onlyOwner" to ensure that only owner is allowed to reveal winners
-    //Hint : Use require to validate if "msg.sender" is equal to the "beneficiary".
+    // Create a modifier named "onlyOwner" to ensure that only owner is allowed to reveal winners
     modifier onlyOwner {
-        // ** Start code here. 2 lines approximately. **
         require(beneficiary == msg.sender);
         _;
-        //** End code here. **
     }
     
     
@@ -128,14 +106,8 @@ contract Auction {
 
             uint winnerId = currentItem.itemTokens[randomIndex];
                 
-            /* Part 1 Task 6. Assign the winners.
-            Hint." bidders[winnerId] " will give you the person object with the winnerId.
-            you need to assign the address of the person obtained above to winners[id] */
-
-            // ** Start coding here *** 1 line approximately.
-            
+            /* Assign the winners. */
             winners[id] = bidders[winnerId].addr;
-            //** end code here*
                 
             }
         }
